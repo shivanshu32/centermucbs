b8b82195c5c6989214cd972c306de09584377a71
AbscodInformatics <shivanshupathak@Shivanshus-MacBook-Air.local>
2025-01-26 02:50:34 +0530

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 import Hero from "./components/hero";
-import Navbar from "./components/navbar";
 import About from "./components/about";
 import Services from "./components/services";
 import Trainingprograms from "./components/trainingprograms";
 import Whychooseus from "./components/whychooseus";
 import Upcomingtraining from "./components/upcomingtraining";
 import Previoustraining from "./components/previoustraining";
-import Footer from "./components/footer";
 import Contact from "./components/contact";
 
 export default function Home() {
   return (
    <div>
- <Navbar />
+
  <Hero />
  <About />
  <Services />
@@ -21,7 +19,7 @@ export default function Home() {
  <Upcomingtraining />
  <Previoustraining />
  <Contact />
- <Footer />    
+ 
 
     </div>
   );
