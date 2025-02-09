c16a1160e44483db5a2a51e1bcbd37e4acc5bfde
AbscodInformatics <shivanshupathak@Shivanshus-MacBook-Air.local>
2025-02-09 22:43:35 +0530

    save

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -3,7 +3,7 @@ import React, { useRef }  from 'react'
 import emailjs from '@emailjs/browser';
 
 export default function Contact() {
-    const form = useRef('');
+    const form = useRef<HTMLFormElement | string>('');
 
     const sendEmail = (e : React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
